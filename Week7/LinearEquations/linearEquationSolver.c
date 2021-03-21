7ad817e5022e9de1c0b9300558e800a7ed6dd73f
Josefine1409 <josefine@robl.dk>
2021-03-21 19:16:15 +0100

    QR-decomposition changed: Pointer to double have become double instead, thus not trying to read and write to random non-initialized location. Should work now.

--- Week7/LinearEquations/linearEquationSolver.c
+++ Week7/LinearEquations/linearEquationSolver.c
@@ -7,8 +7,8 @@
 void gramSchmidtDecomposition(gsl_matrix* A, gsl_matrix* R){
 	// Initializing m, the size of matrix R is (m, m)
 	int m = A->size2;
-	// Defining a pointer to a double for later use for the dot product value
-	double* dotProduct;
+	// Defining a double for later use for the dot product value
+	double dotProduct;
 	// Gram-Schmidt decomposition
 	for (int i = 0; i < m; i++) {
 		// Create a view of the i-th column of the matrix
@@ -23,8 +23,8 @@ void gramSchmidtDecomposition(gsl_matrix* A, gsl_matrix* R){
 			// Create a view of the j-th colum of the matrix
 			gsl_vector_view colJ = gsl_matrix_column(A, j);
 			// Compute the dot product between A_i and A_j
-			gsl_blas_ddot(&col.vector, &colJ.vector, dotProduct);
-			gsl_matrix_set(R, i, j, *dotProduct);
+			gsl_blas_ddot(&col.vector, &colJ.vector, &dotProduct);
+			gsl_matrix_set(R, i, j, dotProduct);
 			// Set A_j to be A_j - q_i * R_{ij}
 			gsl_vector_scale(&col.vector, gsl_matrix_get(R, i, j)); // Updates &col.vector
 			gsl_vector_sub(&colJ.vector, &col.vector); // Updates &colJ.vector <(write more here, like that in the i for-loop)>
@@ -39,13 +39,24 @@ void gramSchmidtDecomposition(gsl_matrix* A, gsl_matrix* R){
 //             return backsub(R, x);
 //     }
 
-void print_matrix(gsl_matrix* R){
-	for (int i = 0; i < R->size1; i++) {
-		for (int j = 0; j < R->size2; j++) {
-			printf("%f      ", gsl_matrix_get(R, i, j));
+void printMatrix(gsl_matrix* M){
+	for (int i = 0; i < M->size1; i++) {
+		for (int j = 0; j < M->size2; j++) {
+			printf("%f	", gsl_matrix_get(M, i, j));
 		}
 		printf("\n");
 	}
+}
+
+double randomBetweenPlusMinus1(){
+	// Initialize
+	double randomDouble;
+	// For a random seed (random due to dependence on computer's internal clock).
+	//srand(time(NULL));
+	// Generating random double value from random int
+	randomDouble = (double)rand()/RAND_MAX*2.0-1.0; // Double in range -1 to 1
+	// Returning the random double value between -1 and 1
+	return randomDouble;
 }
 
 void gramSchmidtSolve(gsl_matrix* Q, gsl_matrix R, gsl_vector* b, gsl_vector* x);
@@ -58,13 +69,17 @@ int main(void){
 	// Assigning entries in A with floats of random values ranging from -1 to 1.
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			gsl_matrix_set(A, i, j, j);
+			gsl_matrix_set(A, i, j, randomBetweenPlusMinus1());
 		}
 	}
-	print_matrix(A);
+	printf("Matrix A before QR-decomposition\n");
+	printMatrix(A);
 	gsl_matrix* R = gsl_matrix_alloc(m, m);
 	gramSchmidtDecomposition(A, R);
-	print_matrix(A);
+	printf("Matrix A after QR-decomposition\n");
+	printMatrix(A);
+	printf("Matrix R after QR-decomposition\n");
+	printMatrix(R);
 	gsl_matrix_free(A);
 	gsl_matrix_free(R);
 	return 0;

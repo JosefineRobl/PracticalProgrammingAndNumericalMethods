3c4be0ea7bbd15a2d68b530b3f78ac4219c56a5d
Josefine1409 <josefine@robl.dk>
2021-06-09 18:09:35 +0200

    Exxercise Math finished

--- Exercises/Math/math.c
+++ Exercises/Math/math.c
@@ -4,7 +4,7 @@
 #include <complex.h>
 #include <string.h>
 
-// Definitions were already defined, thus now out commented
+// Definitions were already defined, thus now out-commented
 // #define M_E 2.7182818284590452354 // e
 // #define M_PI 3.1415926535897932384 // pi
 
@@ -30,7 +30,7 @@ void printExercise(int n){
  * val: Double containing the function to evaluate or the correct value found online.
  */
 void printReal(char funName[], char valType[], double val){
-	printf("%s %s value: %g\n", funName, valType, val);
+	printf("%s %s value: %g.\n", funName, valType, val);
 }
 
 /*
@@ -41,7 +41,7 @@ void printReal(char funName[], char valType[], double val){
  * val: Complex double containing th function to evaluate or the correct value found online.
  */
 void printComplex(char funName[], char valType[], double complex val){
-	printf("%s %s value: %g%+gi\n", funName, valType, crealf(val), cimagf(val));
+	printf("%s %s value: %g%+gi.\n", funName, valType, crealf(val), cimagf(val));
 }
 
 /*
@@ -65,6 +65,7 @@ void print(char funName[], double complex fun, double complex realVal){
 		printComplex(funName, "real", realVal);
 }
 
+
 /*
  * Calculated the precision of a computation of 1./9.
  *
@@ -72,13 +73,17 @@ void print(char funName[], double complex fun, double complex realVal){
  *
  * Return the integer number of digits after decimal point which are still precise.
  */
-int precisionNumber(const char val){
-	// Length of the initial segment containing "0.1", i.e. until it does not have 0.1111111... anymore.
-	return strspn(val, "0.1");
+int precisionNumber(char* str){
+	// Initialization of counter
+	int counter = 0;
+	// Updating the counter with the number of 1's in the number
+	while (str[counter] == '1') counter++;
+	// Returns the counter
+	return counter;
 }
 
 /*
- * Converts a float, a double and a long double to an array of strings the three numbers' digits after the decimal point, which of is taken to be to the 25th decimal.
+ * Converts a float, a double and a long double to an array of strings containing the three numbers digits after the decimal point, which of is taken to be to the 25th decimal.
  *
  * f: Float containing the number as a float.
  * d: Double containing the number as a double.
@@ -86,46 +91,38 @@ int precisionNumber(const char val){
  *
  * Returns an array of the three strings formed from the decimals of the three given numbers.
  */
+/* NO LONGER IN USE!!!
 const char toString(float f, double d, long double ld){
 	// Initialize list of strings and strings
-	const char strings[3];
-	char floatString[25];
-	char doubleString[25];
-	char longDoubleString[25];
-	// Translate the numbers after the decimal poit
-        snprintf(floatString, 25, "%.25g", f*pow(10, 25));
-	snprintf(doubleString, 25, "%.25lg", d*pow(10, 25));
-	snprintf(longDoubleString, 25, "%.25Lg", ld*pow(10, 25));
+	char floatString[28];
+	char doubleString[28];
+	char longDoubleString[28];
+	// Translate the numbers to strings
+        snprintf(*floatString, 28, "%.25g", f);
+	snprintf(*doubleString, 28, "%.25lg", d);
+	snprintf(*longDoubleString, 28, "%.25Lg", ld);
+	// Getting strings containing only the numbers after the decimal point, which strings will also be "searchable" by str[i]
+	char* floatStringAfterDecimal = &floatString[2];
+	char* doubleStringAfterDecimal = &doubleString[2];
+	char* longDoubleStringAfterDecimal = &longDoubleString[2];
 	// return the array of strings
+	const char* strings[] = {floatStringAfterDecimal, doubleStringAfterDecimal, longDoubleStringAfterDecimal};
 	return strings;
 }
+*/
 
 /*
- * Calculates the value of 1./9 as float, double and long double and prints the result along with the number of digits after decimal separator, which are still precise.
+ * Hash function.
+ * 
+ * str: String which shall be hashed.
+ * 
+ * returns the hash of str.
  */
-void calculatePrecision(void){
-	// Calculating the values of 1./9 as float, double and long double
-	float x_float = 1./9;
-	double x_double = 1./9;
-	long double x_long_double = 1./9;
-	// Converts the numbers to strings
-	const char strings[3];
-	strcpy(strings, toString(x_float, x_double, x_long_double));
-	// Index of precision in the computation (index of first number !=1 and -2 due to "0." is of length 2)
-	int index_float = precisionNumber(strings[0]);
-	int index_double = precisionNumber(strings[1]);
-	int index_long_double = precisionNumber(strings[2]);
-	// Calls the print function for the precisions
-	printPrecision("float", strings[0], index_float);
-	printPrecision("double", strings[1], index_double);
-	printPrecision("long double", strings[2], index_long_double);
-}
-
-unsigned long hash(char *str){
+unsigned long hash(char* str){
 	unsigned long hash = 5381;
 	int c;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + c; // hash * 33 + c
 	return hash;
 }
 
@@ -133,28 +130,52 @@ unsigned long hash(char *str){
  * Prints the result of a calculation of 1./9 as well as the precision of the calculation.
  *
  * dataType: A string containing the data type in question, i.e. "float", "double" and "long double".
- * formatSpecifier: A string containing the format specifier for the given dataType, i.e. "g", "lg" and "Lg" for float, double and long double respectively..
  * result: String containing the result of the calculation of 1./9.
  * precisionIndex: Integer of the number of digits after the decimal seperator still precise.
- */
-void printPrecision(char dataType[], char result[], int precisionIndex){
-	char formatSpecifier[];
-	switch ((int)hash(dataType)){
-		case (int)hash("float"):
-			formatSpecifier = "%.25g";
-			float num = strtof(result);
-			break;
-		case (int)hash("double"):
-			formatSpecifier = "%.25lg";
-			double num = strtod(result);
-			break;
-		case (int)hash("long double"):
-			formatSpecifier = "%.25Lg";
-			long double num = strtold(result);
+*/
+void printPrecision(char* dataType, char* result, int precisionIndex){
+	char* num;
+	// String comparison without hash function may also be viable, but to be sure of the equality, the hash function have been used. This is also the remains from the time where the if-statement were switch-cases instead, thus the hash function was needed to distinguish the cases
+	if (hash(dataType) == hash("float")) {
+		num = "1.f/9";
+	} else if (hash(dataType) == hash("double")) {
+		num = "1./9";
+	} else if (hash(dataType) == hash("long double")) {
+		num = "1.L/9";
 	}
 	printDash();
-	printf("1./9 for %s: 1./9 = %s", dataType, formatSpecifier, num*pow(10, -25));
-	printf("For %s the precision is down to digit %d after decimal separator", dataType, precisionIndex);
+	printf("1./9 for %s: %s = 0.%s.\n", dataType, num, result);
+	printf("For %s the precision is down to digit %d after decimal separator.\n", dataType, precisionIndex);
+}
+
+/*
+ *  * Calculates the value og 1./9 as float, double and long double, and prints the result along with the number of digits after the decimal separator, which are still precise.
+ *   */
+void calculatePrecision(void){
+	// Calculating the values of 1./9 as float, double and long double
+	float xFloat = 1.f/9;
+	double xDouble = 1./9;
+	long double xLongDouble = 1.L/9;
+	// Initialize list of strings
+	char floatString[28];
+	char doubleString[28];
+	char longDoubleString[28];
+	// Translate the numbers to strings
+	snprintf(floatString, 28, "%.25g", xFloat);
+	snprintf(doubleString, 28, "%.25lg", xDouble);
+	snprintf(longDoubleString, 28, "%.25Lg", xLongDouble);
+	// Getting strings containing only the numbers after the decimal point, which will also be searchable by str[i]
+	char* floatStringAfterDecimal = &floatString[2];
+	char* doubleStringAfterDecimal = &doubleString[2];
+	char* longDoubleStringAfterDecimal = &longDoubleString[2];
+	// Index of precision in the computation (index of the first number !=1, thus the actual number of 1's in the calculation)
+	int indexFloat = precisionNumber(floatStringAfterDecimal);
+	int indexDouble = precisionNumber(doubleStringAfterDecimal);
+	int indexLongDouble = precisionNumber(longDoubleStringAfterDecimal);
+	// Calls the print function for the precisions
+	printPrecision("float", floatStringAfterDecimal, indexFloat);
+	printPrecision("double", doubleStringAfterDecimal, indexDouble);
+	printPrecision("long double", longDoubleStringAfterDecimal, indexLongDouble);
 }
 
 /*
@@ -172,8 +193,11 @@ int main(void){
 	print("exp(i)", cexp(I), 0.54030231+0.84147098*I);
 	print("i^e", pow(I, M_E), -0.42821977-0.90367462*I);
 	print("i^i", pow(I, I), 0.20787958);
+	
 	// Exercise 2
+	printf("\n");
 	printExercise(2);
 	calculatePrecision();
+	
 	return 0;
 }

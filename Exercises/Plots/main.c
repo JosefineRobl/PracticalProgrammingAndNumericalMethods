18bece89948a3503d26b8b70d0ffedb957ce4ec1
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-14 13:38:42 +0200

    printGamma function back inside main function

--- Exercises/Plots/main.c
+++ Exercises/Plots/main.c
@@ -8,10 +8,6 @@
 #include "erfApproximation.h"
 #include "gammaApproximation.h"
 
-void printGamma(double x){
-	printf("%10g\t%10g\t%10g\t%10g\n", x, tgamma(x), gsl_sf_gamma(x), gammaStirlingApproximation(x));
-}
-
 int main(int argc, char** argv){
 	// The main function shall take 1 argument being 1 for error function and 2 for gamma function
 	assert(argc == 2);
@@ -21,6 +17,9 @@ int main(int argc, char** argv){
 			printf("%10g\t%10g\t%10g\t%10g\n", x, erf(x), gsl_sf_erf(x), erfApproximation(x));
 		}
 	} else if (arg == 2) {
+		void printGamma(double x){
+			printf("%10g\t%10g\t%10g\t%10g\n", x, tgamma(x), gsl_sf_gamma(x), gammaStirlingApproximation(x));
+		}
 		double x;
 		for (x = -4.9; x <= -4.1; x += 1./8) {
 			printGamma(x);

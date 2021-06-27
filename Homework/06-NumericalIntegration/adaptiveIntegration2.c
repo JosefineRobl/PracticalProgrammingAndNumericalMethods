caabbfac8c9bf4c4ab82c3db66ed9ddfde258358
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-27 14:55:19 +0200

    Comments added in the code

--- Homework/06-NumericalIntegration/adaptiveIntegration2.c
+++ Homework/06-NumericalIntegration/adaptiveIntegration2.c
@@ -3,64 +3,116 @@
 #include <assert.h>
 #include "adaptiveIntegration2.h"
 
-static double A, B; // the left and right integration limit
+/*
+ * DUE TO WSL NOT BEING ABLE TO WORK WITH NESTED FUNCTIONS AND INSTEAD RESULT IN SEGMENTATION FAULT. THE ACTUAL NESTED CODE IS COMMENTED OUT, SUCH THAT IT MIGHT STILL BE TESTED ON ANOTHER SYSTEM, BUT DUE TO THIS, THE FUNCTIONS ARE MOVED OUT HERE.
+ */
 
-static double fClenshawCurtis(double f(double), double t){// Variable transformation for Clenshaw-Curtis
+// Not accesible from other files, and only here due to moved nested functions needing a and b
+static double A, B;
+
+// Variable transformation for Clenshaw-Curtis
+static double fClenshawCurtis(double f(double), double t){
 	return f( (A + B)/2 + (A - B)/2 * cos(t) ) * sin(t) * (B - A)/2;
 }
 
+// If only a is INFINITY: Using converted integral from table in "Numerical Integration" PDF (eq. 62)
 static double gOnlyLowerLimitInf(double f(double), double t){
 	return f( B - (1 - t)/t ) / pow(t, 2);
 }
 
+// If only b is INFINITY: Using converted integral from table in "Numerical Integration" PDF (eq. 60)
 static double gOnlyUpperLimitInf(double f(double),double t){// variable transformation formula
 	return f( A + (1 - t)/t ) / pow(t, 2);
 }
 
+// If both a and b are INFINITY: Using converted integral from table in "Numerical Integration" PDF (eq. 58)
 static double gBothLimitsInf(double f(double), double t){
 	return ( f((1-t)/t) + f(-(1-t)/t) ) / pow(t, 2);
 }
 
-double wrap24( double f(double),double a, double b, double delta, double epsilon, double f2, double f3, int nrec, int variableTransformationFormula){//wrapper calls F(f,t)
-	assert(nrec < 99);
+/*
+ * END OF FUNCTIONS MOVED FROM NESTED DUE TO WSL.
+ */
+
+/*
+ * Adaptively and recursively integrates a function.
+ * 
+ * f: Function to be integrated. Takes a double as input and returns a double (the function value at that point).
+ * a: Double containing the lower integration limit.
+ * b: Double containing the upper integration limit.
+ * delta: Double containing the absolute accuracy goal.
+ * epsilon: Double containing the relative accuracy goal.
+ * f2: Double containing the evaluated function for a specified x value.
+ * f3: Double containing the evaluated function for a specified x value (other than the above).
+ * recursionLimit: Integer determining the maximum number of recusions before exiting the program with an error.
+ * variableTransformationFormula: Integer determining the variable transformation formula to use. 0 if no integration limit is infinity, 1 if lower integration limit is infinity, 2 if upper integration limit is infinity, and 3 if both lower and upper integration limit is infinity.
+ * 
+ * returns a double containing the recursively and adaptively integrated value of the function in the integration limit.
+ */
+double recursiveIntegrate( double f(double),double a, double b, double delta, double epsilon, double f2, double f3, int nrec, int variableTransformationFormula){//wrapper calls F(f,t)
+	// Initialization of the upper and lower x-values used in the surrounding functions below
 	double xValLowerLimit = a + (b - a)/6,
 	       xValUpperLimit = a + 5*(b - a)/6;
+	// Initialization of functions surrounding func2 and func3 using 'Numerical Integration' PDF eq. 51 and 48
 	double f1, f4;
 	switch (variableTransformationFormula) {
 		case 1:
+			// Lower limit infinity transformation
 			f1 = gOnlyLowerLimitInf(f, xValLowerLimit);
 			f4 = gOnlyLowerLimitInf(f, xValUpperLimit);
 			break;
 		case 2:
+			// Upper limit infinity transformation
 			f1 = gOnlyUpperLimitInf(f, xValLowerLimit);
 			f4 = gOnlyUpperLimitInf(f, xValUpperLimit);
 			break;
 		case 3:
+			// Both limits infinity transformation
 			f1 = gBothLimitsInf(f, xValLowerLimit);
 			f4 = gBothLimitsInf(f, xValUpperLimit);
 			break;
 		case 4:
+			// Clenshaw-Curtis transformation
 			f1 = fClenshawCurtis(f, xValLowerLimit);
 			f4 = fClenshawCurtis(f, xValUpperLimit);
 			break;
 		default:
+			// No variable transformation since variableTransformationFormula == 0
 			f1 = f(xValLowerLimit);
 			f4 = f(xValUpperLimit);
 	}
+	// Initialization of higher and lower order rules (Q and q respectively)
 	double Q = (2*f1 + f2 + f3 + 2*f4) / 6 * (b - a);
 	double q = (f1 + f2 + f3 + f4) / 4 * (b - a);
+	// Initialization of the error and the tolerance
 	double tolerance = delta + epsilon*fabs(Q);
 	double error = fabs(Q - q);
-	if(error < tolerance) return Q;
-	else {
-		double Q1 = wrap24(f, a, (a+b)/2, delta/sqrt(2), epsilon, f1, f2, nrec + 1, variableTransformationFormula);
-		double Q2 = wrap24(f, (a+b)/2, b, delta/sqrt(2), epsilon, f3, f4, nrec + 1, variableTransformationFormula);
+	// Assert the number of recursion
+	assert(nrec < 99);
+	// Return value: Depending on the found error vs. the tolerance keep doing recursions or return the found integral value
+	if(error < tolerance)
+		return Q;
+	} else {
+		double Q1 = recursiveIntegrate(f, a, (a+b)/2, delta/sqrt(2), epsilon, f1, f2, nrec + 1, variableTransformationFormula);
+		double Q2 = recursiveIntegrate(f, (a+b)/2, b, delta/sqrt(2), epsilon, f3, f4, nrec + 1, variableTransformationFormula);
 		return Q1 + Q2;
 	}
 }
 
-double integrate2(double f(double), double a, double b, double delta, double epsilon, int variableTransformationFormula){
-	// Save the limits in the file-scope variables
+/*
+ * Integrates a function with limits possibly being infinity, using two sub-divisions of the integration interval, and an adaptive and recursive technique.
+ * 
+ * f: Function to be integrated. Taks a double as input and returns a double (the function value at that point).
+ * a: Double containing the lower integration limit.
+ * b: Double containing the upper integration limit.
+ * delta: Double containing the absolute accuracy goal.
+ * epsilon: Double containing the relative accuracy goal.
+ * variableTransformationFormula: Integer determining the variable transformation formula to use. 0 if no integration limit is infinity, 1 if lower integration limit is infinity, 2 if upper integration limit is infinity, and 3 if both lower and upper integration limit is infinity.
+ * 
+ * returns a double containing the recursively and adaptively integrated value of the function in the integration limit.
+ */
+double integrate(double f(double), double a, double b, double delta, double epsilon, int variableTransformationFormula){
+	// Initialize field-scope variables with the integration limits
 	A=a; B=b;
 	// Set new limits for use in the transformation formulas
 	switch (variableTransformationFormula) {
@@ -76,39 +128,68 @@ double integrate2(double f(double), double a, double b, double delta, double eps
 		case 4:
 			a = 0; b = M_PI;
 	}
+	// Initialized func2 and func3 from eq. 51 and 48 in the 'Numerical Integration' PDF
 	double xValLowerLimit = a + 2*(b - a)/6,
 	       xValUpperLimit = a + 4*(b - a)/6;
 	double f2, f3;
 	switch (variableTransformationFormula) {
 		case 1:
+			// Lower limit infinity transformation
 			f2 = gOnlyLowerLimitInf(f, xValLowerLimit);
 			f3 = gOnlyLowerLimitInf(f, xValUpperLimit);
 			break;
 		case 2:
+			// Upper limit infinity transformation
 			f2 = gOnlyUpperLimitInf(f, xValLowerLimit);
 			f3 = gOnlyUpperLimitInf(f, xValUpperLimit);
 			break;
 		case 3:
+			// Both limits infinity transformation
 			f2 = gBothLimitsInf(f, xValLowerLimit);
 			f3 = gBothLimitsInf(f, xValUpperLimit);
 			break;
 		case 4:
+			// Clenshaw-Curtis transformation
 			f2 = fClenshawCurtis(f, xValLowerLimit);
 			f3 = fClenshawCurtis(f, xValUpperLimit);
 			break;
-
 		default:
+			// No variable transformation since variableTransformationFormula == 0
 			f2 = f(xValLowerLimit);
 			f3 = f(xValUpperLimit);
 	}
+	// Initialize the number of recursions
 	int nrec = 0;
-	return wrap24(f, a, b, 2*delta, 2*epsilon, f2, f3, nrec, variableTransformationFormula);
+	// Begin recursion
+	return recursiveIntegrate(f, a, b, 2*delta, 2*epsilon, f2, f3, nrec, variableTransformationFormula);
 }
 
+/*
+ * Integrates a function using the Clenshaw-Curtis transformation of the integral.
+ * 
+ * f: Function to be integrated. Taks a double as input and returns a double (the function value at that point).
+ * a: Double containing the lower integration limit.
+ * b: Double containing the upper integration limit.
+ * delta: Double containing the absolute accuracy goal.
+ * epsilon: Double containing the relative accuracy goal.
+ * 
+ * returns a double containing the integrated value of the function in the integration limit using the Clenshaw-Curtis transformation.
+ */
 double ClenshawCurtisIntegrate(double f(double), double a, double b, double delta, double epsilon){
 	return integrate2(f, a, b, delta, epsilon, 4);
 }	
 
+/*
+ * Integrates a function with one or more limits possibly being infinity, using two sub-divisions of the integration interval, and an adaptive and recursive technique.
+ * 
+ * f: Function to be integrated. Takes a double as input and returns a double (the function value at the point).
+ * a: Double containing the lower integration limit.
+ * b: Double containing the upper integration limit.
+ * delta: Double containing the absolute accuracy goal.
+ * epsilon: Double containing the relative accuracy goal.
+ * 
+ * returns a double containing the recursively and adaptively integrated value of the function in the integration limit.
+ */
 double generalisedIntegrator2(double f(double), double a, double b, double delta, double epsilon){
 	int variableTransformationFormula;
 	if (isinf(a) != 0) {

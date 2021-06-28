ee44402fe9a76c2297702f7ff66df7759d0ede49
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 18:02:43 +0200

    Update artificialNeuralNetworks.h

--- Homework/10-NeuralNetwork/artificialNeuralNetworks.h
+++ Homework/10-NeuralNetwork/artificialNeuralNetworks.h
@@ -1,4 +1,7 @@
-#include <gsl/gsl_vector.h> // Implements vectors
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
 
 /*
  * Include guard
@@ -7,14 +10,14 @@
 #define haveANNh
 
 /*
- *
+ * Structure containing the network.
  */
 typedef struct{
-	int n;
-	double (*f)(double t);
-	double (*dfdt)(double t);
-	double (*F)(double t);
-	gsl_vector* params;
+	int n; // Number of neurons
+	double (*f)(double t); // Activation function
+	double (*dfdt)(double t); // Derivative of activation function 
+	double (*F)(double t); // Antiderivative of activation function
+	double* params; // Parameters to be tuned during training
 } artificialNeuralNetwork;
 
 /*
@@ -33,12 +36,12 @@ artificialNeuralNetwork* annAlloc(int n, double (*f)(double t), double (*dfdt)(d
 void annFree(artificialNeuralNetwork* network);
 
 /*
- *
+ * Respons function.
  */
 double annResponse(artificialNeuralNetwork* network, double x);
 
 /*
- *
+ * Derivative of respons function. Not needed for tuning due to use of same parameters for both function, derivative and antiderivative, but still need it to approximate derivative of tabulated function.
  */
 double annDerivative(artificialNeuralNetwork* network, double x);
 
@@ -50,7 +53,7 @@ double annIntegral(artificialNeuralNetwork* network, double x);
 /*
  * Train the artificial neural network on xs, ys.
  */
-void annTrain(artificialNeuralNetwork* network, gsl_vector* xs, gsl_vector* ys);
+void annTrain(artificialNeuralNetwork* network, int nx, double* xs, double* ys);
 
 /*
  * Ending definition inside include guard

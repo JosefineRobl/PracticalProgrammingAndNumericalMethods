9e17879d5dcc57a87cbec28774930ec4c6eacd93
Josefine1409 <josefine@robl.dk>
2021-06-28 19:52:34 +0200

    Test

---
+++ Homework/08-Roots/rootFinding.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_matrix.h>
+
+/*
+ * ...
+ *
+ * PER DMITRI THE PARAMETERS ARE
+ * function f: takes the input vector x and fills fx=f(x)
+ * vector x: on entry -- the stating point; upon exit -- the approximation to the root
+ * double epsilon: the accuracy goal; on exit the condition ||f(x)|| < epsilon should be satisfied
+ */
+void newton(void f(gsl_vector* x, gsl_vector* fx), gsl_vectoor* x, double epsilon);

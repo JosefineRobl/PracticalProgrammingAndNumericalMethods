6be6737ea097f698ae4acf2a677ecddc15e1a8c3
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 09:29:02 +0200

    Update quadraticSpline.h

--- Homework/01-Interpolation/quadraticSpline.h
+++ Homework/01-Interpolation/quadraticSpline.h
@@ -1,1 +1,30 @@
+#include <assert.h>
+#include <gsl/gsl_vetor.h>
+#include "binarySearch.h"
 
+typedef struct{
+	gsl_vector* x;
+	gsl_vector* y;
+	gsl_vector* b;
+	gsl_vector* c;
+} quadraticSpline;
+
+quadraticSpline* quadraticSplineAlloc(gsl_vector* x, gsl_vector* y);
+
+/*
+ * Evaulates interpolation in point z (see table in notes).
+ */
+double quadraticSplineEval(quadraticSpline* s, double z);
+
+/*
+ * Function that evaluates the derivative in point z.
+ */
+double quadraticSplineEval(quadraticSpline* s, double z);
+
+/*
+ * Function that evaluates integral from x[0] to z.
+ */
+double quadraticSplineEvalDerivative(quadraticSpline* s, double z);
+
+//Function for freeing allocated memory
+void quadraticSplineFree(quadraticSpline* s);

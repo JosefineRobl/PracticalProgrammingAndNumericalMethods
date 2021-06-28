5352d94435810a827eccc9602c7f757314bce99d
Josefine1409 <josefine@robl.dk>
2021-06-28 13:13:45 +0200

    Homework 9

---
+++ Homework/09-Minimization/quasiNewton.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <gsl/gsl_vector.h>
+
+/*
+ * ...
+ *
+ * F: Objective function.
+ * x: Pointer to vector; on input is the starting point, while on exit is the approximation to the root.
+ * epsilon: Double containing the accuracy goal; on exit the absolute value of the gradient should be less than epsilon.
+ */
+void quasiNewton(double F(gsl_vector* x), gsl_vector* x, double epsilon){
+	// Initialization
+	int n = x->size; // Dimension of vector x
+	int steps = 0; // Steps taken all in all for finding the minimum
+	int goodSteps = 0; // Good steps taken in search of the minimum
+	int badSteps = 0; // Bad steps taken in search of the minimum
+	//
+	gsl_matrix* B = gsl_matrix_alloc(n, n);
+	gsl_vector* gradient = gsl_vector_alloc(n);
+
+	gsl_vector* dx = gsl_vector_alloc(n);
+	gsl_vector* xNew = gsl_vector_alloc(n);
+	gsl_vector* gradientNew = gsl_vector_alloc(n);
+
+	gsl_vector* dGradient = gsl_vector_alloc(n);
+	gsl_vector* u = gsl_vector_alloc(n);
+
+	gsl_matrix_set_identity(B);
+	numericGradient(f, x, gradient);
+
+	double fx = f(x);
+	double fxNew;
+
+	while (steps < 1000) {
+		gsl_blas_dgemv(CblasNoTrans, -1, B, gradient, 0, dx);
+
+		if (gsl_blas_dnrm2(dx) < delta*gsl_blas_dnrm2(x)) {
+			fprintf(stderr, "Error in function quasiNewton: |dx < delta*|x|.\n");
+			break;
+		}
+		if (gsl_blas_dnrm2(gradient) < epsilon) {
+			fprintf(stderr, "Error in function quasiNewton: |gradient| < epsilon.\n");
+			break;
+		}
+		
+		double lambda = 1;
+
+		while (1) {
+			gsl_vector_memcpy(xNew, x);
+			gsl_vector_add(xNew, dx);
+			fxNew = f(xNew);
+
+			double sTGradient;
+			gsl_blas_ddot(dx, gradient, &sTGradient);
+
+			if (fxNew < fx + 0.01*sTGradient) {
+				goodSteps++;
+				break;
+			}
+			
+			if (lambda < delta) {
+				badSteps++;
+				break;
+			}
+
+			lambda *= 0.5;
+			gsl_vector_scale(dx, 0.5);
+		}
+		
+		// ...
+
+		steps++;
+}

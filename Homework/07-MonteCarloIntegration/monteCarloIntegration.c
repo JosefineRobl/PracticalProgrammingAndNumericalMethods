ec42a1828e4cd9b3661088a814e462b5e1218020
Josefine1409 <josefine@robl.dk>
2021-06-28 10:38:59 +0200

    Homework 7 finished

--- Homework/07-MonteCarloIntegration/monteCarloIntegration.c
+++ Homework/07-MonteCarloIntegration/monteCarloIntegration.c
@@ -134,6 +134,79 @@ complex quasiMonteCarloIntegration(int dim, double f(int dim, double* x), double
 	return integral + I*error;
 }
 
+/*
+ * Recursive stratified sampling.
+ */
+double strataMonteCarloIntegration(int dim, double f(int dim, double* x), double* a, double* b, double acc, double epsilon, double nReuse, double meanReuse){
+	int N = 16*dim;
+	double V = 1;
+	for (int i = 0; i < dim; i++) {
+		V *= b[i] - a[i];
+	}
+	double x[dim];
+	// Initialize means and other lists
+	double meanLeft[dim], meanRight[dim], mean = 0;
+	int nLeft[dim], nRight[dim];
+	for (int d = 0; d < dim; d++) {
+		meanLeft[d] = 0;
+		meanRight[d] = 0;
+		nLeft[d] = 0;
+		nRight[d] = 0;
+	}
+	
+	for (int i = 0; i < N; i++) {
+		// Update the list of x's
+		for (int d = 0; d < dim; d++) {
+			x[d] = a[d] + RANDOM*(b[d] - a[d]);
+		}
+		// Evaluate function
+		double fx = f(dim, x);
+		// Updating the lists
+		for (int d = 0; d < dim; d++) {
+			if (x[d] > (a[d] + b[d])/2) {
+				meanRight[d] += fx;
+				nRight[d]++;
+			} else {
+				meanLeft[d] += fx;
+				nLeft[d]++;
+			}
+		}
+		mean += fx;
+	}
+	// Calculate mean by deviding the number with the number of points
+	mean /= N;
+	for (int d = 0; d < dim; d++) {
+		meanLeft[d] /= nLeft[d];
+		meanRight[d] /= nRight[d];
+	}
+	// Initialize variable for the maximum variance and its index, and update these
+	double maximumVariance = 0;
+	int maximumVarianceIndex = 0;
+	for (int d = 0; d < dim; d++) {
+		double variance = fabs(meanRight[d] - meanLeft[d]);
+		if (variance > maximumVariance) {
+			maximumVariance = variance;
+			maximumVarianceIndex = d;
+		}
+	}
+
+	// Calculating the integrals using the reusable points and the integration of the left and right
+	double integral = V*(N*mean + meanReuse*nReuse)/(N + nReuse),
+	       error = V*fabs(meanReuse - mean),
+	       tolerance = acc + fabs(integral)*epsilon;
+	if (error < tolerance) return integral;
+	double aNew[dim], bNew[dim]; // New integration limits
+	for (int d = 0; d < dim; d++) {
+		// Initialize new integration limits
+		aNew[d] = a[d];
+		bNew[d] = b[d];
+	}
+	aNew[maximumVarianceIndex] = (a[maximumVarianceIndex] + b[maximumVarianceIndex])/2; // New limits for split integral
+	bNew[maximumVarianceIndex] = aNew[maximumVarianceIndex]; // New limits for split integrals
+	return strataMonteCarloIntegration(dim, f, a, bNew, acc/sqrt(2), epsilon, nLeft[maximumVarianceIndex], meanLeft[maximumVarianceIndex])
+		+ strataMonteCarloIntegration(dim, f, aNew, b, acc/sqrt(2), epsilon, nRight[maximumVarianceIndex], meanRight[maximumVarianceIndex]);
+}
+
 
 /*
  * Prints the integral alongside the calculated and the theoretical solution.
@@ -163,22 +236,34 @@ void printSolutionToIntegral(char* integral, complex calculatedSolution, complex
 }
 
 /*
+ * Integrals to test.
+ */
+double functionOverSqrtX(int d, double* t){
+	return 1./sqrt(t[0]);
+}
+double functionComplexExponential(int d, double *t){
+	return cexp(I*t[0]);
+}
+double functionSingularIntegral(int d, double* t){
+	return 1./((1 - cos(t[0])*cos(t[1])*cos(t[2]))*pow(M_PI, d));
+}
+
+/*
  * Calculating some interesting integrals with the Monte-Carlo routine.
  */
-void testIntegralsForPartAB(int monteCarloMethod){
-	assert(monteCarloMethod == 1 || monteCarloMethod == 2);
+void testIntegrals(int monteCarloMethod){
+	assert((monteCarloMethod >= 1) && (monteCarloMethod <= 3));
 	
 	// Initializes two strings with the integral and its solution respectively
 	char* integralOverSqrtX = "∫0π  dx/√(x)";
 	double integralOverSqrtXApproxSolution = 2*sqrt(M_PI);
-	// Initializes the function for integration, the integration limits, the number of points and the dimensions
-	double functionOverSqrtX(int d, double* t) {
-		return 1./sqrt(t[0]);
-	}
+	// Initializes the integration limits, the number of points and the dimensions
 	double a[] = {0};
 	double b[] = {M_PI};
 	int N = 1e7;
 	int dim = 1;
+	double accuracy = 1e-3,
+	       epsilon = 1e-3;
 	// Prints the integral as the exercise subtitle
 	printExerciseSubtitle(integralOverSqrtX);
 	// Calculating the integral
@@ -189,6 +274,9 @@ void testIntegralsForPartAB(int monteCarloMethod){
 			break;
 		case 2:
 			integralOverSqrtXCalculatedSolution = quasiMonteCarloIntegration(dim, functionOverSqrtX, a, b, N);
+			break;
+		case 3:
+			integralOverSqrtXCalculatedSolution = strataMonteCarloIntegration(dim, functionOverSqrtX, a, b, accuracy, epsilon, 0, 0);
 	}
 	// Prints the integral, the calculated solution to the integral and the theoretical solution to the integral
 	printSolutionToIntegral(integralOverSqrtX, integralOverSqrtXCalculatedSolution, integralOverSqrtXApproxSolution);
@@ -196,14 +284,13 @@ void testIntegralsForPartAB(int monteCarloMethod){
 	// Initializes two strings with the integral and its solution respectively
 	char* integralComplexExponential = "∫0π exp(ix) dx";
 	complex integralComplexExponentialApproxSolution = 2*I;
-	// Initializes the function for integration, the integration limits, the number of points and the dimensions
-	double functionComplexExponential(int d, double* t) {
-		return cexp(I*t[0]);
-	}
+	// Initializes the integration limits, the number of points and the dimensions
 	double aComplexExponential[] = {0};
 	double bComplexExponential[] = {M_PI};
 	N = 1e7;
 	dim = 1;
+	accuracy = 1e-3;
+	epsilon  = 1e-3;
 	// Prints the integral as the exercise subtitle
 	printf("\n");
 	printExerciseSubtitle(integralComplexExponential);
@@ -215,30 +302,32 @@ void testIntegralsForPartAB(int monteCarloMethod){
 			break;
 		case 2:
 			integralComplexExponentialCalculatedSolution = quasiMonteCarloIntegration(dim, functionComplexExponential, aComplexExponential, bComplexExponential, N);
+			break;
+		case 3:
+			integralComplexExponentialCalculatedSolution = strataMonteCarloIntegration(dim, functionComplexExponential, aComplexExponential, bComplexExponential, accuracy, epsilon, 0, 0);
 	}
 	// Prints the integral, the calculated solution to the integral and the theoretical solution to the integral
 	printSolutionToIntegral(integralComplexExponential, integralComplexExponentialCalculatedSolution, integralComplexExponentialApproxSolution);
 }
 
-void testPartAB(int monteCarloMethod){
-	assert(monteCarloMethod == 1 || monteCarloMethod == 2);
+void test(int monteCarloMethod){
+	assert((monteCarloMethod >= 1) && (monteCarloMethod <= 3));
 
 	// Calculating som interesting integrals with the Monte-Carlo routine
-	testIntegralsForPartAB(monteCarloMethod);
+	testIntegrals(monteCarloMethod);
 
 	printf("\n");
 
 	// Initializes two strings with the singular integral and its solution respectively
 	char* singularIntegral = "∫0π dx/π ∫0π dy/π ∫0π dz/π 1/[1 - cos(x)cos(y)cos(z)]";
 	double singularIntegralApproxSolution = pow(tgamma(1./4), 4)/(4*pow(M_PI, 3));
-	// Initializes the function for integration, the integration limits, the number of points and the dimensions
-	double functionSingularIntegral(int d, double* t) {
-		return 1./((1 - cos(t[0])*cos(t[1])*cos(t[2]))*pow(M_PI, d));
-	}
+	// Initializes the integration limits, the number of points and the dimensions
 	double a[] = {0, 0, 0};
 	double b[] = {M_PI, M_PI, M_PI};
 	int N = 1e7;
 	int dim = 3;
+	double accuracy = 1e-3,
+	       epsilon = 1e-3;
 	// Prints the singular integral as the exercise subtitle
 	printExerciseSubtitle(singularIntegral);
 	// Calculating the integral
@@ -249,11 +338,17 @@ void testPartAB(int monteCarloMethod){
 			break;
 		case 2:
 			singularIntegralCalculatedSolution = quasiMonteCarloIntegration(dim, functionSingularIntegral, a, b, N);
+			break;
+		case 3:
+			singularIntegralCalculatedSolution = strataMonteCarloIntegration(dim, functionSingularIntegral, a, b, accuracy, epsilon, 0, 0);
 	}
 	// Prints the integral, the calculated solution to the integral and the theoretical solution to the integral
 	printSolutionToIntegral(singularIntegral, singularIntegralCalculatedSolution, singularIntegralApproxSolution);
 }
 
+/*
+ * Error comparison function for exercise B.
+ */
 double funForErrorComparison(int dim, double* r){
 	assert(dim == 2);
 	double x = r[0];
@@ -279,7 +374,6 @@ void errorComparisonAB(void){
 		complex resultQuasiMC = quasiMonteCarloIntegration(dim, funForErrorComparison, a, b, N);
 		fprintf(error, "%20d %20g %20g\n", n, cimag(resultPlainMC), cimag(resultQuasiMC));
 	}
-	printf("The scaling of the errors for the plain monte-carlo vs the quasi-random monte-carlo as a function of N are shown in error.png\n");
 	fclose(error);
 }
 
@@ -290,17 +384,19 @@ int main(int argc, char** argv){
 	} else {
 		printExerciseTitle("Exercise A");
 		printf("Integrals calculated using the plain Monte-Carlo integrator.\n");
-		testPartAB(1);
+		test(1);
 		
 		printf("\n");
 		printExerciseTitle("Exercise B");
 		printf("The same integrals as those from exercise A is tested using the quasi-random Monte-Carlo integrator.\n");
-		testPartAB(2);
+		test(2);
 		printExerciseSubtitle("Error comparison");
 		printf("Comparison of the scaling of the error for the quasi-random Monte-Carlo integrator (exercise B) with the pseudo-random Monte-Carlo integrator (exercise A) can be seen at error.png (with the data in error.txt). The error scaling is done with the integral of cos^2(x)*sin^2(y) from 0 to 2*PI for both limits.\n");
 		
 		printf("\n");
 		printExerciseTitle("Exercise C");
+		printf("The same integrals as those from exercise A (and B) is tested using the stratified sampling Monte-Carlo integrator.\n");
+		test(3);
 	}
 	return 0;
 }

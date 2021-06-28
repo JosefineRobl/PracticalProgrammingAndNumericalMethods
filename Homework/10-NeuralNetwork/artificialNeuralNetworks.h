9e6a081e8c67d0b7c2bcfb729ee182bf596a84c5
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 16:44:35 +0200

    Update due to exercise B

--- Homework/10-NeuralNetwork/artificialNeuralNetworks.h
+++ Homework/10-NeuralNetwork/artificialNeuralNetworks.h
@@ -11,17 +11,24 @@
  */
 typedef struct{
 	int n;
-	double (*f)(double);
+	double (*f)(double t);
+	double (*dfdt)(double t);
+	double (*F)(double t);
 	gsl_vector* params;
 } artificialNeuralNetwork;
 
 /*
- *
+ * Allocates space for the artificial neural network.
+ * 
+ * n: Number of neurons in the artificial neural network.
+ * f: Function.
+ * dfdt: Derivative of function.
+ * F: Integral of funtion.
  */
-artificialNeuralNetwork* annAlloc(int n, double (*f)(double));
+artificialNeuralNetwork* annAlloc(int n, double (*f)(double t), double (*dfdt)(double t), double (*F)(double t));
 
 /*
- *
+ * Free memory allocated by artificial neural network.
  */
 void annFree(artificialNeuralNetwork* network);
 
@@ -32,6 +39,16 @@ double annResponse(artificialNeuralNetwork* network, double x);
 
 /*
  *
+ */
+double annDerivative(artificialNeuralNetwork* network, double x);
+
+/*
+ *
+ */
+double annIntegral(artificialNeuralNetwork* network, double x);
+
+/*
+ * Train the artificial neural network on xs, ys.
  */
 void annTrain(artificialNeuralNetwork* network, gsl_vector* xs, gsl_vector* ys);
 

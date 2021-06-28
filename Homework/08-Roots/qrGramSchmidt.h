904720351c49939c3dd171a4bb21cbf089fffa7a
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 18:46:51 +0200

    Update qrGramSchmidt.h

--- Homework/08-Roots/qrGramSchmidt.h
+++ Homework/08-Roots/qrGramSchmidt.h
@@ -2,6 +2,8 @@
 #include <gsl/gsl_matrix.h> // Enables Gnu Scientific Library matrices and some of their functions to be used
 #include <gsl/gsl_blas.h> // Enables Gnu Scientific Library functions for calculations with gsl vectors and matrices.
 
+double norm(gsl_vector* x)
+
 void qrGramSchmidtDecomposition(gsl_matrix* A, gsl_matrix* R);
 
 void qrGramSchmidtSolve(gsl_matrix* Q, gsl_matrix* R, gsl_vector* b, gsl_vector* x);

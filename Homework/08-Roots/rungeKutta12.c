8acc8d435d851d7a86fa116358029a693727f444
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 19:32:58 +0200

    Update rungeKutta12.c

--- Homework/08-Roots/rungeKutta12.c
+++ Homework/08-Roots/rungeKutta12.c
@@ -1,81 +1,105 @@
 #include <stdio.h>
 #include <math.h>
-
-// Definition
-#define FORALL(i) for (int i = 0; i < n; i++)
-#define PRINT(t, y) fprintf(out, "%15g\t", t); FORALL(i) fprintf(out, "%15g\t", y[i]); fprintf (out, "\n")
+#include <stdlib.h>
 
 /*
  * Stepper.
  * 
- * n: Integer containing the size of the vector y.
  * f: The function from dy/dt = f(t, y).
- * t: The current value of the variable.
- * y: The current value y(t) of the sought function.
+ * n: Integer containing the size of the vector y.
+ * x: The current value of the variable.
+ * yx: The current value y(t) of the sought function.
  * h: Double containing the step to be taken.
- * step: The y(t+h)-value.
+ * yh: The y(t+h)-value.
  * dy: The error esimate.
  */
-void rungeKuttaStep12(int n, void f (double t, double y[], double dydt[]), double t, double y[], double h, double step[], double dy[]){
-	double k0[n], k1[n], y1[n];
-	// Evaluate the function to determine k0
-	f(t, y, k0);
-	FORALL(i) y1[i] = y[i] + (0.5*h)*k0[i];
-	// Evaluate the function to determine k1
-	f(t + 0.5*h, y1, k1);
-	FORALL(i) {
-		step[i] = y[i] + h*k1[i];
-		dy[i] = (k1[i] - k0[i])*h;
+void rungeKuttaStep23(void f(int n, double x, double* y, double* dydx), int n, double x, double* yx, double h, double* yh, double* dy) {
+	double k1[n], k2[n], k3[n], k4[n];
+	double yt[n];
+	// Evaluate to find k1
+	f(n, x, yx, k1);
+	for (int i = 0; i < n; i++) {
+		yt[i] = yx[i] + 1./2 * k1[i]*h;
+	}
+	// Evaluate to find k2
+	f(n, x+1./2 * h, yt, k2);
+	for (int i = 0; i < n; i++) {
+		yt[i] = yx[i] + 3./4 * k2[i]*h;
+	}
+	// Evaluate to find k3
+	f(n, x + 3./4 * h, yt, k3);
+	for (int i=0; i < n; i++) {
+		yh[i] = yx[i] + (2./9 * k1[i] + 1./3 * k2[i] + 4./9 * k3[i])*h;
+	}
+	// Evaluate to find k4
+	f(n, x + h, yh, k4);
+	for (int i=0; i<n; i++){
+		yt[i] = yx[i] + (7./24 * k1[i] + 1./4 * k2[i] + 1./3 * k3[i] + 1./8 * k4[i])*h;
+		dy[i] = yh[i] - yt[i];
 	}
 }
 
 /*
  * Driver.
  * 
- * n: Integer containing the size of the vector y.
- * f: The function from dy/dt = f(t, y).
- * a: The start of y.
- * b: The end of y.
- * y: The current value y(t) of the sought function.
- * h: Double containing the step to be taken.
- * delta: Double containing the absolute accuracy goal.
- * epsilon: Double containing the relative accuracy goal.
- * out: Pointer to FILE object to store the points calculated along the way.
  */
-void rungeKuttaDrive12(int n, void f (double t, double y[], double dydt[]), double a, double b, double y[], double h, double delta, double epsilon, FILE* out){
-	// Initialize the time t as the starting point
-	double t = a;
-	// Print values of t and y before run (exercise part B)
-	PRINT(t, y);
+void rungeKuttaDrive23(void f(int n, double x, double* y, double* dydx), int n, double a, double b, double* ya, double* yb, double h, double delta, double epsilon, char* path){
+	// Create file to write to
+	FILE* file = fopen(path, "w");
+	// Initialize variables (mostly) for the step
+	double x;
+	double y[n];
+	double sumNorm, sumError,
+	       error, norm, tolerance;
+	double yh[n];
+	double dy[n];
+	// Initialize the x as the starting point
+	x = a;
+	// Print values of x and y before run
+	fprintf(file, "%20g", x);
+	for(int i = 0; i < n; i++){
+		y[i] = ya[i];
+		fprintf(list, "%20g", y[i]);
+	}
+	fprintf(file, "\n");
 	// Calculate as long as we have not reached the last point yet
-	while (t < b) {
-		if (t + h > b) {
-			h = b - t;
+	while (x < b){
+		if (x + h > b) {
+			h = b - x;
 		}
-		// Initialize vectors and summation variables
-		double steps[n], dy[n],
-		       sumForNorm = 0, sumForError = 0;
 		// Perform step with stepper from above
-	      	rungeKuttaStep12 (n, f, t, y, h, steps, dy);
-		// Calculate the norm of y, the error and the rolerance
-		FORALL(i) {
-			sumForNorm += pow(y[i], 2);
-			sumForError += pow(dy[i], 2);
+		rkstep23(f, n, x, y, h, yh, dy);
+		// Calculate the norm of y, the error and the tolerance
+		sumNorm = 0;
+		sumError = 0;
+		for (int i = 0; i < n; i++){
+			sumError += pow(dy[i], 2);
+			sumNorm += pow(yh[i], 2);
 		}
-		double error = sqrt(sumForError); 
-		double tolerance = (delta + epsilon*sqrt(sumForNorm)) * sqrt(h / (b - a));
+		error = sqrt(sumError);
+		norm = sqrt(sumError);
+		tolerance = (norm*epsilon + delta)*sqrt(h/(b - a));
 		// Compare the error with the tolerance, and just the error by itself
-		if (error < tolerance) {
-			t = t + h;
-			// Update the y-list
-			FORALL(i) y[i] = steps[i];
-			// Print values of t and y after run (exercise part B)
-			PRINT(t, y);
+		if (error < tolerance){
+			x += h;
+			fprintf(list, "%20g", x);
+			for (int i = 0; i < n; i++) {
+				// Update the y-list
+				y[i] = yh[i];
+				// Print values of t and y after run
+				fprintf(file, "%20g", y[i]);
+			}
+			fprintf(file, "\n");
 		}
-		if (error > 0) {
-			h *= 0.95*pow(tolerance/error, 0.25); 
-		} else 	{
+		if (err > 0) {
+			h *= 0.95*pow(tol/err, 0.25);
+		} else {
 			h *= 2;
 		}
 	}
+	for (int i = 0; i < n; i++) {
+		yb[i] = yh[i];
+	}
+	// Close file
+	fclose(file);
 }

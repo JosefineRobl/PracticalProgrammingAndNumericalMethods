24ab3e0ac5421671b092592118618f74859199b4
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 19:33:44 +0200

    Update rungeKutta12.h

--- Homework/08-Roots/rungeKutta12.h
+++ Homework/08-Roots/rungeKutta12.h
@@ -1,30 +1,22 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 /*
  * Stepper.
  * 
- * n: Integer containing the size of the vector y.
  * f: The function from dy/dt = f(t, y).
- * t: The current value of the variable.
- * y: The current value y(t) of the sought function.
+ * n: Integer containing the size of the vector y.
+ * x: The current value of the variable.
+ * yx: The current value y(t) of the sought function.
  * h: Double containing the step to be taken.
- * step: The y(t+h)-value.
+ * yh: The y(t+h)-value.
  * dy: The error esimate.
  */
-void rungeKuttaStep12(int n, void f(double t, double y[], double dydt[]), double t, double y[], double h, double step[], double error[]);
+void rungeKuttaStep23(void f(int n, double x, double* y, double* dydx), int n, double x, double* yx, double h, double* yh, double* dy);
 
 /*
  * Driver.
  * 
- * n: Integer containing the size of the vector y.
- * f: The function from dy/dt = f(t, y).
- * a: The start of y.
- * b: The end of y.
- * y: The current value y(t) of the sought function.
- * h: Double containing the step to be taken.
- * delta: Double containing the absolute accuracy goal.
- * epsilon: Double containing the relative accuracy goal.
- * out: Pointer to FILE object to store the points calculated along the way.
  */
-void rungeKuttaDrive12 (int n, void f(double t, double y[], double dydt[]), double a, double b, double y[], double h, double delta, double epsilon, FILE* out);
+void rungeKuttaDrive23(void f(int n, double x, double* y, double* dydx), int n, double a, double b, double* ya, double* yb, double h, double delta, double epsilon, char* path)

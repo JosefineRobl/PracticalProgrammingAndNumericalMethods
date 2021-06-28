9329be4e17f383651697a06d9178a1a0e659a0e2
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 16:41:57 +0200

    Update for exercise B

--- Homework/10-NeuralNetwork/artificialNeuralNetworks.c
+++ Homework/10-NeuralNetwork/artificialNeuralNetworks.c
@@ -3,18 +3,25 @@
 #include "quasiNewton.h"
 
 /*
- *
+ * Allocates space for the artificial neural network.
+ * 
+ * n: Number of neurons in the artificial neural network.
+ * f: Function.
+ * dfdt: Derivative of function.
+ * F: Integral of funtion.
  */
-artificialNeuralNetwork* annAlloc(int n, double (*f)(double)){
+artificialNeuralNetwork* annAlloc(int n, double (*f)(double t), double (*dfdt)(double t), double (*F)(double t)){
 	artificialNeuralNetwork* network = malloc(sizeof(artificialNeuralNetwork));
 	network->n = n;
 	network->f = f;
+	network->dfdt = dfdt;
+	network->F = F;
 	network->params = gsl_vector_alloc(3*n);
 	return network;
 }
 
 /*
- *
+ * Free memory allocated by artificial neural network.
  */
 void annFree(artificialNeuralNetwork* network){
 	gsl_vector_free(network->params);
@@ -28,6 +35,7 @@ double annResponse(artificialNeuralNetwork* network, double x){
 	double sum = 0;
 	// Initialization of weights
 	double a, b, w;
+	// Set weights and the suum
 	for (int i = 0; i < network->n; i++) {
 		a = gsl_vector_get(network->params, 3*i + 0);
 		b = gsl_vector_get(network->params, 3*i + 1);
@@ -40,6 +48,40 @@ double annResponse(artificialNeuralNetwork* network, double x){
 /*
  *
  */
+double annDerivative(artificialNeuralNetwork* network, double x){
+	double sum = 0; 
+	// Initialization of weights
+	double a, b, w;
+	// Set weights and the sum
+	for (int i = 0; i < network->n; i++) {
+		a = gsl_vector_get (network->parameters, 3*i + 0); 
+		b = gsl_vector_get (network->parameters, 3*i + 1); 
+		w = gsl_vector_get (network->parameters, 3*i + 2); 
+		sum += network->derivative((x - a)/b)*w;
+	}
+	return sum; 
+}
+
+/*
+ *
+ */
+double annIntegral(artificialNeuralNetwork* network, double x){
+	double sum = 0; 
+	// Initialization of weights
+	double a, b, w;
+	// Set weights and the sum
+	for (int i = 0; i < network->n; i++) {
+		a = gsl_vector_get (network->parameters, 3*i + 0); 
+		b = gsl_vector_get (network->parameters, 3*i + 1); 
+		w = gsl_vector_get (network->parameters, 3*i + 2); 
+		sum += network->integral((x - a)/b)*w;
+	}
+	return sum; 
+}
+
+/*
+ * Train the artificial neural network on xs, ys.
+ */
 void annTrain(artificialNeuralNetwork* network, gsl_vector* xs, gsl_vector* ys){
 	double costFunction(gsl_vector* params){
 		gsl_vector_memcpy(network->params, params);
@@ -49,7 +91,7 @@ void annTrain(artificialNeuralNetwork* network, gsl_vector* xs, gsl_vector* ys){
 			xi = gsl_vector_get(xs, i);
 			yi = gsl_vector_get(ys, i);
 			fi = annResponse(network, xi);
-			sum += (fi - yi)*(fi - yi);
+			sum += pow(fi - yi, 2);
 		}
 		// Average
 		return sum/(x->size);

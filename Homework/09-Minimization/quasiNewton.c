e806d9db9a8f2ee83d61f1828d5c5e134501bb85
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 13:25:10 +0200

    Update quasiNewton.c

--- Homework/09-Minimization/quasiNewton.c
+++ Homework/09-Minimization/quasiNewton.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include "quasi_newton.h"
+double delta = sqrt (DBL_EPSILON); 
+
+/*
+ * Calculates the numeric gradient.
+ */
+void numericGradient(double f(gsl_vector* x), gsl_vector* x, gsl_vector* gradient){
+	double fx = f(x);
+	for (int i = 0; i < x->size; i++){
+		double dx;
+		double x_i = gsl_vector_get (x, i);
+		if (fabs (x_i) < sqrt (delta)) {
+			dx = delta;
+		} else {
+			dx = fabs (x_i)*delta;
+		}
+		gsl_vector_set(x, i, x_i + dx); 
+		gsl_vector_set(gradient, i, (f(x) - fx)/dx);
+		gsl_vector_set(x, i, x_i); 
+	}
+}
 
 /*
  * ...
@@ -31,7 +56,7 @@ void quasiNewton(double F(gsl_vector* x), gsl_vector* x, double epsilon){
 	double fx = f(x);
 	double fxNew;
 
-	while (steps < 1000) {
+	while (steps < 10000) {
 		gsl_blas_dgemv(CblasNoTrans, -1, B, gradient, 0, dx);
 
 		if (gsl_blas_dnrm2(dx) < delta*gsl_blas_dnrm2(x)) {

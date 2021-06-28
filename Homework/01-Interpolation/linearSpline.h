476479f8a860f52377a325a6326de020a7b910b0
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 10:02:20 +0200

    Update linearSpline.h

--- Homework/01-Interpolation/linearSpline.h
+++ Homework/01-Interpolation/linearSpline.h
@@ -1,30 +1,25 @@
-#include <assert.h>
-#include <gsl/gsl_vector.h>
+#include <gsl/gsl_vector.h> // Gnu Scientific Library with vectors
+#include <assert.h> // For assertions throughtout the code
 #include "binarySearch.h"
 
-typedef struct{
-	gsl_vector* x;
-	gsl_vector* y;
-	gsl_vector* b;
-	gsl_vector* c;
-} quadraticSpline;
-
-quadraticSpline* quadraticSplineAlloc(gsl_vector* x, gsl_vector* y);
+/*
+ * ...
+ *
+ * x:
+ * y:
+ * z:
+ *
+ * Returns a double containing the spline.
+ */
+double linearInterpolation(gsl_vector x, gsl_vector y, double z);
 
 /*
- * Evaulates interpolation in point z (see table in notes).
+ * ...
+ *
+ * x:
+ * y:
+ * z:
+ *
+ * Returns a double containing the result of the integration of the spline on x[0] to z.
  */
-double quadraticSplineEval(quadraticSpline* s, double z);
-
-/*
- * Function that evaluates the derivative in point z.
- */
-double quadraticSplineEval(quadraticSpline* s, double z);
-
-/*
- * Function that evaluates integral from x[0] to z.
- */
-double quadraticSplineEvalDerivative(quadraticSpline* s, double z);
-
-//Function for freeing allocated memory
-void quadraticSplineFree(quadraticSpline* s);
+double linearInterpolationIntegration(gsl_vector x, gsl_vector y, double z);

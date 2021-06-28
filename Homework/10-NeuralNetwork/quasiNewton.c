616da552af2efbb93c9207b4f2c74fc950d5c275
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-28 17:57:01 +0200

    Update quasiNewton.c

--- Homework/10-NeuralNetwork/quasiNewton.c
+++ Homework/10-NeuralNetwork/quasiNewton.c
@@ -1,132 +1,117 @@
-#include <stdio.h>
-#include <math.h>
-#include <float.h>
-#include <gsl/gsl_matrix.h>
-#include <gsl/gsl_vector.h>
-#include <gsl/gsl_blas.h>
-#include "quasiNewton.h"
-double delta = sqrt (DBL_EPSILON); 
+#include<stdio.h>
+#include<math.h>
+#include<float.h>
+static const double h = sqrt(DBL_EPSILON);
+#define For(i) for(int i=0; i<d; i++)
+#define ID_set(B) For(i) For(j) B[i][j]=0; For(i) B[i][i]=1;
 
-/*
- * Calculates the numeric gradient.
- */
-void numericGradient(double f(gsl_vector* x), gsl_vector* x, gsl_vector* gradient){
-	double fx = f(x);
-	for (int i = 0; i < x->size; i++){
+double norm(int d, double* x){
+	double sum = 0;
+	For(i){
+		sum += x[i]*x[i];
+	}
+	return sqrt(sum);
+}
+
+void gradient(int d, double f(int d, double* x), double* x, double* grad){
+	For(i){
+		double fx = f(d,x);
 		double dx;
-		double x_i = gsl_vector_get (x, i);
-		if (fabs (x_i) < sqrt (delta)) {
-			dx = delta;
+		double xi = x[i];
+		if (fabs(xi)<h) {
+			dx = h;
 		} else {
-			dx = fabs (x_i)*delta;
+			dx = fabs(xi)*h;
 		}
-		gsl_vector_set(x, i, x_i + dx); 
-		gsl_vector_set(gradient, i, (f(x) - fx)/dx);
-		gsl_vector_set(x, i, x_i); 
+		x[i] += dx;
+		grad[i] = (f(d, x) - fx)/dx;
+		x[i] = xi;
 	}
 }
 
 /*
- * ...
- *
- * F: Objective function.
- * x: Pointer to vector; on input is the starting point, while on exit is the approximation to the root.
- * epsilon: Double containing the accuracy goal; on exit the absolute value of the gradient should be less than epsilon.
- * 
- * return the number of steps.
+ * Different quasiNewton that from Homework 9, since I could not get GSL vectors and non-nested functions (due to WSL) to collaborate.
  */
-int quasiNewton(double f(gsl_vector* x), gsl_vector* x, double epsilon){
-	// Initialization
-	int n = x->size; // Dimension of vector x
-	int steps = 0; // Steps taken all in all for finding the minimum
-	int goodSteps = 0; // Good steps taken in search of the minimum
-	int badSteps = 0; // Bad steps taken in search of the minimum
-	//
-	gsl_matrix* B = gsl_matrix_alloc(n, n);
-	gsl_vector* gradient = gsl_vector_alloc(n);
+int quasiNewton(int d, double f(int, double*), double* x, double acc){
+	int nsteps = 0;
+	double B[d][d]; // inverse Hessian
+	ID_set(B); // start with B being the identity
+	double gradf[d];
+	gradient(d, f, x, gradf); // gradient stored in gradf
+	double fx = f(d, x);
+	double fz;
 
-	gsl_vector* dx = gsl_vector_alloc(n);
-	gsl_vector* xNew = gsl_vector_alloc(n);
-	gsl_vector* gradientNew = gsl_vector_alloc(n);
+	double maxnsteps = 10000;
 
-	gsl_vector* dGradient = gsl_vector_alloc(n);
-	gsl_vector* u = gsl_vector_alloc(n);
+	while(1){
+		nsteps++;
+		if(nsteps > maxnsteps) break;
+		double normgradf = norm(d, gradf);
+		if(normgradf < acc) break;
 
-	gsl_matrix_set_identity(B);
-	numericGradient(f, x, gradient);
-
-	double fx = f(x);
-	double fxNew;
-
-	while (steps < 10000) {
-		steps++; // Update the steps variable
-		// Perform matrix multiplication
-		gsl_blas_dgemv(CblasNoTrans, -1, B, gradient, 0, dx);
-		// Error handling
-		if (gsl_blas_dnrm2(dx) < delta*gsl_blas_dnrm2(x)) {
-			fprintf(stderr, "Error in function quasiNewton: |dx < delta*|x|.\n");
+		double Dx[d]; // Newtons step
+		For(i) {
+			Dx[i]=0;
+			For(j) {
+				Dx[i] -= B[i][j]*gradf[j];
+			}
+		}
+		double normx = norm(d, x);
+		double normDx = norm(d, Dx);
+		if (normDx < h*normx) {
 			break;
 		}
-		if (gsl_blas_dnrm2(gradient) < epsilon) {
-			fprintf(stderr, "Error in function quasiNewton: |gradient| < epsilon.\n");
-			break;
-		}
-		
+
+		// Start back tracking line search
 		double lambda = 1;
-		while (1) { // true til we break the loop
-			gsl_vector_memcpy(xNew, x);
-			gsl_vector_add(xNew, dx);
-			fxNew = f(xNew);
-
-			double sTGradient;
-			gsl_blas_ddot(dx, gradient, &sTGradient);
-			// Update good and bad step variable
-			if (fxNew < fx + 0.01*sTGradient) {
-				goodSteps++;
+		double z[d];
+		while(1){
+			For(i) z[i] = x[i] + Dx[i];
+			fz = f(d,z);
+			double DxTG = 0;
+			For(i) DxTG += Dx[i]*gradf[i]; // dot product
+			double alpha = 0.01;
+			if (fz < fx + alpha*DxTG) {
 				break;
 			}
-			if (lambda < delta) {
-				badSteps++;
+			if (lambda < h) {
+				ID_set(B);
 				break;
 			}
-			// Update lambda
-			lambda *= 0.5;
-			gsl_vector_scale(dx, 0.5);
+			lambda*=0.5;
+			For(i) Dx[i]*=0.5;
 		}
-		// Update the gradient vectors
-		numericGradient(f, xNew, gradientNew);
-		gsl_vector_memcpy(dGradient, gradientNew);
-		gsl_blas_daxpy(-1, gradient , dGradient);
-		// Copy the derivative due to use of vector in function
-		gsl_vector_memcpy(u, dx);
-		gsl_blas_dgemv(CblasNoTrans, -1, B, dGradient, 1, u);
-		// Define new variables to keep dot product of gradients
-		double sTDGradient, uTDGradient;
-		gsl_blas_ddot(dx, dGradient, &sTDGradient);
-		if (fabs(sTDGradient) > 1e-12) {
-			gsl_blas_ddot(u, dGradient, &uTDGradient);
-			double gamma = uTDGradient/(2*sTDGradient);
-			gsl_blas_daxpy (-gamma, dx, u);
-			gsl_blas_dger (1./sTDGradient, u, dx, B);
-			gsl_blas_dger (1./sTDGradient, dx, u, B);
+		double gradfz[d];
+		gradient(d, f, z, gradfz);
+		double y[d];
+		For(i) {
+			y[i] = gradfz[i] - gradf[i]; // y = gradf(x+Dx)-gradf(x)
 		}
-		// Copy the value from xNew into the original x variable, and likewise for the gradient
-		gsl_vector_memcpy(x, xNew);
-		gsl_vector_memcpy(gradient, gradientNew);
+		double u[d];
+		For(i) {
+			u[i] = Dx[i];
+			For(j) {
+				u[i]-=B[i][j]*y[j];
+			}
+		} // u=Dx-B*y
+		double uTy=0;
+		For(i) {
+			uTy += u[i]*y[i];
+		}
+		if(fabs(uTy) > 1e-12){
+			For(i) {
+				For(j) {
+					B[i][j] += u[i]*u[j]/uTy;
+				}
+			}
+		}
+		For(i) {
+			x[i]=z[i];
+		}
+		For(i) {
+			gradf[i]=gradfz[i];
+		}
+		fx = fz;
 	}
-	// Free all matrices and vectors
-	gsl_matrix_free(B);
-	gsl_vector_free(gradient);
-	gsl_vector_free(dx);
-	gsl_vector_free(xNew);
-	gsl_vector_free(gradientNew);
-	gsl_vector_free(dGradient);
-	gsl_vector_free(u);
-	// Print the number of steps, the good steps, the bad steps and the fucntion value at the minimum
-	FILE* quasiNewtonFile = fopen("quasiNewtonPrints.txt", "w");
-	fprintf(quasiNewtonFile, "quasi_newton : steps = %i, good steps = %i, bad steps = %i\n", steps, goodSteps, badSteps); 
-	fprintf(quasiNewtonFile, "quasi_newton : function value at min %g \n", fx);
-	fclose(quasiNewtonFile);
-	
-	return steps;
+	return nsteps;
 }

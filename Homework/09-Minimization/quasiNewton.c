d05ac23f2cc735cea7bcab8d6cb3d058ab5b98a4
Josefine1409 <josefine@robl.dk>
2021-06-28 13:38:00 +0200

    Test

--- Homework/09-Minimization/quasiNewton.c
+++ Homework/09-Minimization/quasiNewton.c
@@ -57,8 +57,10 @@ void quasiNewton(double F(gsl_vector* x), gsl_vector* x, double epsilon){
 	double fxNew;
 
 	while (steps < 10000) {
+		steps++; // Update the steps variable
+		// Perform matrix multiplication
 		gsl_blas_dgemv(CblasNoTrans, -1, B, gradient, 0, dx);
-
+		// Error handling
 		if (gsl_blas_dnrm2(dx) < delta*gsl_blas_dnrm2(x)) {
 			fprintf(stderr, "Error in function quasiNewton: |dx < delta*|x|.\n");
 			break;
@@ -69,30 +71,57 @@ void quasiNewton(double F(gsl_vector* x), gsl_vector* x, double epsilon){
 		}
 		
 		double lambda = 1;
-
-		while (1) {
+		while (1) { // true til we break the loop
 			gsl_vector_memcpy(xNew, x);
 			gsl_vector_add(xNew, dx);
 			fxNew = f(xNew);
 
 			double sTGradient;
 			gsl_blas_ddot(dx, gradient, &sTGradient);
-
+			// Update good and bad step variable
 			if (fxNew < fx + 0.01*sTGradient) {
 				goodSteps++;
 				break;
 			}
-			
 			if (lambda < delta) {
 				badSteps++;
 				break;
 			}
-
+			// Update lambda
 			lambda *= 0.5;
 			gsl_vector_scale(dx, 0.5);
 		}
-		
-		// ...
-
-		steps++;
+		// Update the gradient vectors
+		numericGradient(F, xNew, gradientNew);
+		gsl_vector_memcpy(dGradient, gradientNew);
+		gsl_blas_daxpy(-1, gradient , dgradient);
+		// Copy the derivative due to use of vector in function
+		gsl_vector_memcpy(u, dx);
+		gsl_blas_dgemv(CblasNoTrans, -1, B, dGradient, 1, u);
+		// Define new variables to keep dot product of gradients
+		double sTDGradient, uTDGradient;
+		if (fabs(sTDGradient) > 1e-12) {
+			gsl_blas_ddot(u, dGradient, &uTDGradient);
+			double gamma = uTDGradient/(2*sTDGradient);
+			gsl_blas_daxpy (-gamma, dx, u);
+			gsl_blas_dger (1./sTdgradient, u, dx, B);
+			gsl_blas_dger (1./sTdgradient, dx, u, B);
+		}
+		// Copy the value from xNew into the original x variable, and likewise for the gradient
+		gsl_vector_memcpy(x, xNew);
+		gsl_vector_memcpy(gradient, gradientNew);
+	}
+	// Free all matrices and vectors
+	gsl_matrix_free(B);
+	gsl_vector_free(gradient);
+	gsl_vector_free(dx);
+	gsl_vector_free(xNew);
+	gsl_vector_free(gradientNew);
+	gsl_vector_free(dGradient);
+	gsl_vector_free(u);
+	// Print the number of steps, the good steps, the bad steps and the fucntion value at the minimum
+	FILE* quasiNewtonFile = fopen("quasiNewtonPrints.txt", "w");
+	fprintf(quasiNewtonFile, "quasi_newton : steps = %i, good steps = %i, bad steps = %i\n", steps, good_steps, bad_steps); 
+	fprintf(quasiNewtonFile, "quasi_newton : function value at min %g \n", fx);
+	fclose(quasiNewtonFile);
 }

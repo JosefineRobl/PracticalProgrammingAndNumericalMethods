fca7fa2edddda27e039d9078de37b8982b9fa344
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-29 11:11:51 +0200

    Update integratorTridivision.c

--- ExamProject/integratorTridivision.c
+++ ExamProject/integratorTridivision.c
@@ -76,8 +76,13 @@ double recursiveIntegrateTridivision(double f(double), double a, double b, doubl
 	// Initialization of the error and the tolerance
 	double tolerance = delta + epsilon*fabs(Q);
 	double error = fabs(Q - q);
+	// Assert
+	if (nrec !> 0) {
+		printf("Returned Q due to nrec");
+		return Q;
+	}
 	// Return value: Depending on the found error vs. the tolerance keep doing recursions or return the found integral value
-	if ((error < tolerance) || (nrec == 0)) {
+	if (error < tolerance) { //((error < tolerance) || (nrec == 0)) {
 		return Q;
 	} else {
 		double deltaNew = delta/sqrt(3);

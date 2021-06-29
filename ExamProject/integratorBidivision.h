38aae00d7ca57bf893794ca13ebb85b948b39378
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-29 09:39:04 +0200

    Create integratorBidivision.h

---
+++ ExamProject/integratorBidivision.h
@@ -0,0 +1,20 @@
+#ifndef haveIntegratorBidivision
+#define haveIntegratorBidivision
+
+#include <stdio.h>
+#include <math.h>
+
+/*
+ * Integrates a function with one or more limits possibly being infinity, using two sub-divisions of the integration interval, and an adaptive and recursive technique.
+ * 
+ * f: Function to be integrated. Takes a double as input and returns a double (the function value at the point).
+ * a: Double containing the lower integration limit.
+ * b: Double containing the upper integration limit.
+ * delta: Double containing the absolute accuracy goal.
+ * epsilon: Double containing the relative accuracy goal.
+ * 
+ * returns a double containing the recursively and adaptively integrated value of the function in the integration limit.
+ */
+double generalisedIntegratorBidivision(double f(double), double a, double b, double delta, double epsilon);
+
+#endif

10902f53db2684c48feb2be35968d3d1d90a9648
Josefine Bjørndal Robl <josefine@robl.dk>
2021-06-29 08:33:38 +0200

    Update integrator5.h

--- ExamProject/integrator5.h
+++ ExamProject/integrator5.h
@@ -3,5 +3,9 @@
 
 double adapt24 (  double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc, double second_funcVal, int integrationLimit );
 double adapt (    double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc );
+double adapt24_old ( double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc, double second_funcVal, double third_funcVal, int numOfRecursions, double* integrationError  );
+double adapt_old ( double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc, double* integrationError );
+double integrate( double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc );
+double integrate_old( double func(double), double leftEndPt, double rightEndPt, double absAcc, double relAcc, double*  integrationError )
 
 #endif //NUMERICAL_INTEGRATION_INTEGRATION_TRI_H

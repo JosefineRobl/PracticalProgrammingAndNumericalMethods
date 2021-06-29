91136f1ebf314870b3569ab2f697da10c4ec0a00
Josefine1409 <josefine@robl.dk>
2021-06-29 08:10:23 +0200

    Test

--- ExamProject/main4.c
+++ ExamProject/main4.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <gsl/gsl_integration.h>
 
-#include "integrator.h"
+#include "integrator4.h"
 
 void printExercise(char* exercise){
 	printf("==================== Exercise %s ====================\n", exercise);
@@ -93,56 +93,6 @@ void testAndPrintIntegrate(double f(double), char* functag, double a, double b, 
 	}
 }
 
-void testAndPrintIntegrateGSL(double f(double, void*), char* functag, double a, double b, double delta, double epsilon, double trueVal, int integrator, int manySignificantDigits){
-	assert((integrator >= 0) && (integrator <= 3));
-	// Initialize call variable
-	calls = 0;
-	// Initialize variables for GSL integration rutine
-	double limit = 10000,
-	       alpha = 1.0,
-	       result, error;
-	gsl_integration_workspace* w = gsl_integration_workspace_alloc(limit);
-	gsl_function G;
-	G.function = f;
-	G.params = &alpha;
-	char* integrationType;
-	// Perfom integration
-	switch (integrator) {
-		case 0:
-			gsl_integration_qags(&G, a, b, delta, epsilon, limit, w, &result, &error);
-			integrationType = "GSL QAGS rutine";
-			break;
-		case 1:
-			gsl_integration_qagil(&G, b, delta, epsilon, limit, w, &result, &error);
-			integrationType = "GSL QAGI rutine (lower limit inf: QAGIL)";
-			break;
-		case 2:
-			gsl_integration_qagiu(&G, a, delta, epsilon, limit, w, &result, &error);
-			integrationType = "GSL QAGI rutine (upper limit inf: QAGIU)";
-			break;
-		case 3:
-			gsl_integration_qagi(&G, delta, epsilon, limit, w, &result, &error);
-			integrationType = "GSL QAGI rutine (both limits inf: QAGI)";
-	}
-	// Calculate the estimated error
-	double estimatedError = delta + fabs(result)*epsilon,
-	       actualError = fabs(result - trueVal);
-	// Print the found values
-	printf("----------\n");
-	printf("The integration of %s from %g to %g using %s:\n", functag, a, b, integrationType);
-	if (manySignificantDigits) {// 1 = true, 0 = false
-		printf("\t Found value     = %.25lg.\n", result);
-	} else {
-		printf("\t Found value     = %g.\n", result);
-	}
-	printf("\t Exact value     = %g.\n", trueVal);
-	printf("\t Estimated error = %g.\n", estimatedError);
-	printf("\t Actual error    = %g.\n", actualError);
-	printf("\t Number of calls = %d.\n", calls);
-	// Free the GSL workspace
-	gsl_integration_workspace_free(w);
-}
-
 int main(void){
 	// Absolute and relative accuracy goals
 	double delta = 1e-4, epsilon = 1e-4;
@@ -162,15 +112,12 @@ int main(void){
 	// fun3
 	printSubtext("Calculating integral of exp(-x) from 0 to infinity using own and GSL rutines");
 	testAndPrintIntegrate(fun3, "exp(-x)", 0, INFINITY, delta, epsilon, 1, "adaptive and recursive integration with bi-division", 0);
-	testAndPrintIntegrateGSL(&fun3GSL, "exp(-x)", 0, INFINITY, delta, epsilon, 1, 2, 0);
 	// fun4
 	printSubtext("Calculating integral of exp(x) from -infinity to 0 using own and GSL rutines");
 	testAndPrintIntegrate(fun4, "exp(x)", -INFINITY, 0, delta, epsilon, 1, "adaptive and recursive integration with bi-division", 0);
-	testAndPrintIntegrateGSL(&fun4GSL, "exp(-x)", -INFINITY, 0, delta, epsilon, 1, 1, 0);
 	// fun5
 	printSubtext("Calculating integral of 1/(1+x²) from -infinity to infinity using own and GSL rutines");
 	testAndPrintIntegrate(fun5, "1/(1+x²)", -INFINITY, INFINITY, delta, epsilon, M_PI, "adaptive and recursive integratioon with bi-division", 0);
-	testAndPrintIntegrateGSL(&fun5GSL, "1/(1+x²)", -INFINITY, INFINITY, delta, epsilon, M_PI, 3, 0);
 	
 	return 0;
 }
